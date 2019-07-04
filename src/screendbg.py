affe40d2791d08ce92462e581161835beba9dd02
André Wagner <andre.nho@gmail.com>
2019-07-04 19:25:33 -0300

    .

--- src/screendbg.py
+++ src/screendbg.py
@@ -3,6 +3,19 @@ import curses
 from screen import Screen
 
 class ScreenDebug(Screen):
+    
+    def __init__(self):
+        Screen.__init__(self)
+        self.stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(True)
 
     def run(self):
         pass
+
+    def __del__(self):
+        curses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
