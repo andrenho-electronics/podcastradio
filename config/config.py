058c26b730a1e198cf5ec22323afffcb3b12555b
André Wagner <andre.nho@gmail.com>
2019-07-14 10:41:50 -0300

    .

--- config/config.py
+++ config/config.py
@@ -7,45 +7,47 @@ from dataclasses import *
 @dataclass
 class Config:
     podcasts:         List[str] = field(default_factory=list)
-    keep_episodes:    int = 5
-    database_file:    str = '/var/db/podcastradio/podcastradio.db'
-    download_path:    str = '/var/db/podcastradio/download'
-    image_path:       str = '/var/db/podcastradio/images'
-    download_threads: int = 3
+    keep_episodes:    int = None
+    database_file:    str = None
+    download_path:    str = None
+    image_path:       str = None
+    download_threads: int = None
 
-    def read_config_file(self, filename):
+    def read_config_file(self, filename, create_directories=True):
         config = configparser.ConfigParser(delimiters=('=',), allow_no_value=True)
         if len(config.read(filename)) == 0:
             logging.warning('File ' + filename + ' not found.')
             return self
+        self.podcasts = self.__load_podcasts(config)
+        self.keep_episodes    = self.__parse_field(config, 'config', 'keep_episodes', 5, int)
+        self.download_path    = self.__parse_field(config, 'config', 'download_path', '/var/db/podcastradio/download')
+        self.image_path       = self.__parse_field(config, 'config', 'image_path', '/var/db/podcastradio/images')
+        self.database_file    = self.__parse_field(config, 'config', 'database_file', '/var/db/podcastradio/podcastradio.db')
+        self.download_threads = self.__parse_field(config, 'config', 'download_threads', 3, int)
+        if create_directories:
+            self.__create_directories()
+
+    def __load_podcasts(self, config):
+        podcasts = []
         if config.has_section('podcasts'):
             for key in config['podcasts']:
                 podcast_name = key
                 value = config['podcasts'][key]
                 if value:
                     podcast_name += '=' + value
-                self.podcasts.append(podcast_name)
+                podcasts.append(podcast_name)
+        return podcasts
+
+    def __parse_field(self, config, section, variable, default_value, process=None):
         try:
-            self.keep_episodes = int(config['config']['keep_episodes'])
+            value = config[section][variable]
+            if process is not None:
+                value = process(value)
+            return value
         except KeyError:
-            pass
-        try:
-            self.download_path = config['config']['download_path']
-            os.makedirs(self.download_path, exist_ok=True)
-        except KeyError:
-            pass
-        try:
-            self.image_path = config['config']['image_path']
-            os.makedirs(self.image_path, exist_ok=True)
-        except KeyError:
-            pass
-        try:
-            self.database_file = config['config']['database_file']
-            os.makedirs(os.path.dirname(self.database_file), exist_ok=True)
-        except KeyError:
-            pass
-        try:
-            self.download_threads = int(config['config']['download_threads'])
-        except KeyError:
-            pass
-        return self
+            return default_value
+
+    def __create_directories(self):
+        os.makedirs(self.download_path, exist_ok=True)
+        os.makedirs(self.image_path, exist_ok=True)
+        os.makedirs(os.path.dirname(self.database_file), exist_ok=True)
--- config/test_config.py
+++ config/test_config.py
@@ -4,14 +4,14 @@ from config import Config
 class TestConfig(unittest.TestCase):
 
     def test_default_config(self):
-        cfg = Config().read_config_file('test/empty_config.ini')
+        cfg = Config().read_config_file('test/empty_config.ini', False)
         self.assertTrue(cfg is not None)
         def fail_cfg():
             cfg = read_config_file('test/non_existing.ini')
         self.assertRaises(Exception, fail_cfg)
 
     def test_load_config(self):
-        cfg = Config().read_config_file('test/download.ini')
+        cfg = Config().read_config_file('test/download.ini', False)
         self.assertEqual(cfg.podcasts, ['http://localhost/op1'])
         self.assertEqual(cfg.keep_episodes, 2)
         self.assertEqual(cfg.download_path, 'download')
@@ -19,9 +19,9 @@ class TestConfig(unittest.TestCase):
 
     def test_corruped_ini(self):
         def fail_cfg():
-            cfg = Config().read_config_file('test/corrupted.ini')
+            cfg = Config().read_config_file('test/corrupted.ini', False)
         self.assertRaises(Exception, fail_cfg)
 
     def test_equal_sign(self):
-        cfg = Config().read_config_file('test/equalsign.ini')
+        cfg = Config().read_config_file('test/equalsign.ini', False)
         self.assertEquals(['http://localhost:8080/op1=123'], cfg.podcasts)
