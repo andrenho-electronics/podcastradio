d69f2877418507129278b840ee2a800fbdecc6ce
André Wagner <WagnerAndre@JohnDeere.com>
2019-07-16 08:09:29 -0300

    .

--- pr_podcast/__main__.py
+++ pr_podcast/__main__.py
@@ -3,9 +3,16 @@ import pr_podcast.pr_podcast as pr_podcast
 import common.config as config
 import common.db as db
 import logging
+import logging.handlers
+import os
 import time
 
 logging.basicConfig(level='INFO')
+if os.name == 'posix':
+    root_logger = logging.getLogger()
+    root_logger.handlers = []
+    root_logger.addHandler(logging.handlers.SysLogHandler(address='/dev/log/'))
+
 cfg = config.Config().read_config_file('podcastradio.ini')
 db  = db.open_database(cfg)
 pm  = pr_podcast.PodcastManager(cfg, db)
